ab326c7ee2623d423d98e232d655036eae4da805
NikolayIliev87 <103068553+NikolayIliev87@users.noreply.github.com>
2023-07-11 13:00:45 +0300

    static dir update to include and public folder

--- backend/settings.py
+++ backend/settings.py
@@ -187,6 +187,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'public'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
